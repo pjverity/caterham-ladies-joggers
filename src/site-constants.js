5158a40aa3f2c132feb9df1beb7550bc90a11019
Paul Verity <paul.verity@v-home.co.uk>
2018-03-31 16:41:23 +0100

    caterham-ladies-joggers-#4: Use NGINX to proxy requests to backends to avoid GKE loadbalancer quotas
    Resolves #4

--- src/site-constants.js
+++ src/site-constants.js
@@ -4,7 +4,7 @@ const INDEX_META_DESCRIPTION = 'Home of Ladies Run Club in Caterham, Surrey';
 
 const FACEBOOK_URL = '/caterhamladiesjoggers';
 const SITE_EMAIL_ADDRESS = 'admin@caterhamladiesjoggers.co.uk';
-const ENQUIRIES_API_URL = 'http://api.caterhamladiesjoggers.co.uk/enquiries/v2/club-enquiry/emails/';
+const ENQUIRIES_API_URL = 'http://www.caterhamladiesjoggers.co.uk/enquiries/v2/club-enquiry/emails/';
 
 module.exports = {
 	INDEX_TITLE,
